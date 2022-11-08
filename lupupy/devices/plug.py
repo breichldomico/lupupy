a3e75013ee2adef811121fa06b47ec564b9494b6
Bernhard Reichl <b.reichl@domico.at>
2022-11-08 17:07:13 +0100

    add plug

--- lupupy/devices/plug.py
+++ lupupy/devices/plug.py
@@ -1,27 +1,27 @@
-"""Lupusec switch device."""
+"""Lupusec plug device."""
 
 from lupupy.devices import LupusecDevice
 import lupupy.constants as CONST
 
 
-class LupusecSwitch(LupusecDevice):
+class LupusecPlug(LupusecDevice):
     """Class to add switch functionality."""
 
     def switch_on(self):
         """Turn the switch on."""
-        success = self.set_status(CONST.STATUS_ON_INT)
+        success = self.set_status(CONST.STATUS_PLUG_ON_INT)
 
         if success:
-            self._json_state['status'] = CONST.STATUS_ON
+            self._json_state['status'] = CONST.STATUS_PLUG_ON
 
         return success
 
     def switch_off(self):
         """Turn the switch off."""
-        success = self.set_status(CONST.STATUS_OFF_INT)
+        success = self.set_status(CONST.STATUS_PLUG_OFF_INT)
 
         if success:
-            self._json_state['status'] = CONST.STATUS_OFF
+            self._json_state['status'] = CONST.STATUS_PLUG_OFF
 
         return success
 
@@ -32,7 +32,8 @@ class LupusecSwitch(LupusecDevice):
 
         Assume switch is on.
         """
-        return self.status not in (CONST.STATUS_OFF, CONST.STATUS_OFFLINE, STATUS_POWER_OFF)
+        _LOGGER.info("Plug")
+        return self.status not in (CONST.STATUS_PLUG_OFF, CONST.STATUS_OFFLINE, STATUS_POWER_OFF)
 
     @property
     def is_dimmable(self):
